3825f70cec4f73d66ee1811ffda7862d5569f669
SalihaShahid <f200147@cfd.nu.edu.pk>
2023-07-27 21:43:35 +0500

    code indentation and comments

--- facebook/facebook_app/forms.py
+++ facebook/facebook_app/forms.py
@@ -5,55 +5,71 @@ from django.contrib.auth.password_validation import validate_password
 
 class SignUpForm(forms.Form):
     gender_choices = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
     ]
     email = forms.EmailField(label="Email")
     name = forms.CharField(max_length=50)
     password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
-    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
     gender = forms.ChoiceField(choices=gender_choices)
-    profile_picture=forms.ImageField()
+    profile_picture = forms.ImageField()
 
     def clean_password(self):
-        password = self.cleaned_data.get('password')
+        password = self.cleaned_data.get("password")
         validate_password(password)
         return password
 
-	
-
 
 class AuthenticationForm(forms.Form):
-	email=forms.EmailField(label="Email")
-	verification_code=forms.CharField(max_length=6)
+    email = forms.EmailField(label="Email")
+    verification_code = forms.CharField(max_length=6)
+
 
 class LoginForm(forms.Form):
-	email=forms.EmailField(label="Email")
-	password=forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField(label="Email")
+    password = forms.CharField(widget=forms.PasswordInput())
 
 
 class SearchForm(forms.Form):
-	search=forms.CharField(label="")
+    search = forms.CharField(label="")
 
 
 class ChatForm(forms.Form):
-    message=forms.CharField(label="")
+    message = forms.CharField(label="")
+
 
 class TextPostForm(forms.Form):
-    content=forms.CharField(label="")
+    content = forms.CharField(label="")
+
 
 class MediaPostForm(forms.Form):
-    content=forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png','gif','mp4'])])
-    caption=forms.CharField(required=False)
+    content = forms.FileField(
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["jpg", "jpeg", "png", "gif", "mp4"]
+            )
+        ]
+    )
+    caption = forms.CharField(required=False)
+
 
 class CommentForm(forms.Form):
-    comment=forms.CharField()
+    comment = forms.CharField()
+
 
 class TextStoryForm(forms.Form):
-    content=forms.CharField(label="")
+    content = forms.CharField(label="")
+
 
 class MediaStoryForm(forms.Form):
-    content=forms.FileField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png','gif','mp4'])])
-    caption=forms.CharField(required=False)
+    content = forms.FileField(
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["jpg", "jpeg", "png", "gif", "mp4"]
+            )
+        ]
+    )
+    caption = forms.CharField(required=False)
